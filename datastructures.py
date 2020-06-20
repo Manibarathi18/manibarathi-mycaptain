366eee6dfda963ef7a2c6a6fdc890558c3318710
Manibarathi18 <67044119+Manibarathi18@users.noreply.github.com>
2020-06-20 13:17:41 +0530

    datastructures.py
    code to assign values in list,access elements in tuple and delete elements in dictionary.

---
+++ datastructures.py
@@ -0,0 +1,20 @@
+#Assinging elements to different list
+list1=[1,2,3,4,5]
+list2=["abc","def","ghi"]
+list3=[11,22,33,44,55]
+list1.append(6)
+list1
+list2.append("jkl")
+list3.append(66)
+list2
+list3 
+#Accesing elements from tuple
+tup1=(1,2,3,4,5)
+tup1[0]
+tup2=("adc","def","ghi")
+tup2[2]
+#Deleting diffferent dictionary elements
+dic={"a":1,"b":2,"c":3}
+if "a" in dic:
+    del dic["a"]
+print(dic)
